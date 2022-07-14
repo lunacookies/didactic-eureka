ec836c0e8884992f9313a7b10e6e295e7bbc22f5
Luna Razzaghipour <aramisnoah@gmail.com>
2022-07-14 13:13:39 +1000

    Change format of TAC to resemble Arm assembly

--- src/tac.rs
+++ src/tac.rs
@@ -14,7 +14,7 @@ pub struct Register(pub u16);
 impl fmt::Debug for Instr {
 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
 		match self {
-			Instr::Const { dst, val } => write!(f, "{dst:?} = \x1b[92m{val}\x1b[0m"),
+			Instr::Const { dst, val } => write!(f, "{dst:?} = \x1b[92m#{val}\x1b[0m"),
 			Instr::Add { dst, lhs, rhs } => {
 				write!(f, "{dst:?} = \x1b[1;33madd\x1b[0m {lhs:?} {rhs:?}")
 			}
@@ -24,6 +24,6 @@ impl fmt::Debug for Instr {
 
 impl fmt::Debug for Register {
 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		write!(f, "\x1b[36m%{}\x1b[0m", self.0)
+		write!(f, "\x1b[36mr{}\x1b[0m", self.0)
 	}
 }
